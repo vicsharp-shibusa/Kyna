44f120b4409ef879b71ff8de075abb0934198683
Vic Sharp <vicsharp@shibusa.io>
2023-11-23 09:23:20 -0600

    Shelled out Kyna architecture.
    Updated README.md.
    
    Added docs/ folder with a project plan and a draw.io file.

---
+++ src/libs/Kyna.ApplicationServices/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Kyna.ApplicationServices.Tests")]
+namespace Kyna.ApplicationServices;
+
+public static class Constants
+{
+
+}
---
+++ src/libs/Kyna.Infrastructure/Constants.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Kyna.Infrastructure.Tests"),
+    InternalsVisibleTo("Kyna.ApplicationServices")]
+namespace Kyna.Infrastructure;
+
+public static class Constants
+{
+
+}
